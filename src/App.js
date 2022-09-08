23e3244584b57800f6e17b2f888f58d4f5bb5e5a
TheProgrammerGod <suvo.ghosh.965@gmail.com>
2022-09-08 20:09:41 +0530

    CSS Restructuring

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import './App.css';
 import ProjectContainer from "./Components/landing/ProjectContainer";
 import ReactCalender from "./Components/calendar/Calendar";
+import EventContainer from "./Components/landing/Eventcontainer";
 
 function App() {
   return (
     <div className="app-body">
       <ProjectContainer />
       <ReactCalender />
+      <EventContainer/>
     </div>
   );
 }
