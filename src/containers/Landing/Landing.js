03a17df8d0ccc3a22caf9139fa06a49f6f54d979
Shaunak Chandra <aqchandra15@gmail.com>
2022-10-05 14:08:33 +0530

    sidebar done

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,26 +1,23 @@
 import React from "react";
+import "./Landing.css";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import ProjectContainer from "../../components/Projects/ProjectContainer";
 import EventContainer from "../../components/Events/EventContainer";
-import ReactCalender from "../../components/Calendar/Calendar"
-
-
+import ReactCalender from "../../components/Calendar/Calendar";
 
 export default function Landing() {
     return (
-        <div className="app-body">
-          <div className="sidebar-container">
-               <Sidebar/>
-          </div>
-          <div className="body-container">
-               <div className="top-container">
-               <ProjectContainer/>
-               <EventContainer />
-               </div>
-               <div className="bottom-container">
-               <ReactCalender />
-               </div>
-          </div>
+        <div className="landing-body">
+            <Sidebar />
+            <div className="body-container">
+                <div className="left-container">
+                    <ProjectContainer />
+                    <EventContainer />
+                </div>
+                <div className="right-container">
+                    <ReactCalender />
+                </div>
+            </div>
         </div>
     );
 }
