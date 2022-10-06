4c56d3ba4356360d4b9a5e4d684ca315865d621c
Shaunak Chandra <aqchandra15@gmail.com>
2022-10-06 22:30:58 +0530

    header added

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -4,19 +4,24 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import ProjectContainer from "../../components/Projects/ProjectContainer";
 import EventContainer from "../../components/Events/EventContainer";
 import ReactCalender from "../../components/Calendar/Calendar";
+// import Todo from "../../components/ToDoList/Todo";
+import Header from "../../components/Header/Header";
 
 export default function Landing() {
     return (
         <div className="landing-body">
             <Sidebar />
             <div className="body-container">
-              <div className="top-container"></div>
+                <div className="top-container">
+                    <Header />
+                </div>
                 <div className="left-container">
                     <ProjectContainer />
                     <EventContainer />
                 </div>
                 <div className="right-container">
                     <ReactCalender />
+                    {/* <Todo /> */}
                 </div>
             </div>
         </div>
