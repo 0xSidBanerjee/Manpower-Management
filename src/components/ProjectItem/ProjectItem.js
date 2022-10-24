d8805feb0b2250304fd10ddee0927dc4fa864889
Shaunak Chandra <aqchandra15@gmail.com>
2022-10-24 20:13:28 +0530

    project page added

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import coverImg from "../../assets/image/project-item-header-background.png";
 import "./ProjectItem.css"
@@ -11,16 +12,20 @@ export const ProjectItem = ({ data }) => {
                     <h6>{data.domain}</h6>
                     <FontAwesomeIcon className="event-item-ellipsis" icon={faEllipsisVertical} />
                </div>
-               <div className="event-item-body">
+               <div className="project-item-body">
                     <img src={coverImg} alt="cover" />
                     <h5>{data.name}</h5>
-                    <div className="event-item-desc">
+                    <div>
                          <p>Lead by {data.leadBy}</p>
                          <button>{data.date}</button>
                     </div>
                </div>
-               <div className="event-item-footer">
-                    
+               <div className="project-item-footer">
+                    <span>
+                         <FontAwesomeIcon icon={faCircleCheck} className="icon"/>
+                         {data.activeMembers}/{data.totalMembers}
+                         </span>
+
                </div>
           </div>
      );
