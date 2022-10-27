b849389acf885050f4a77b00a75574c6fdd40982
Shaunak Chandra <aqchandra15@gmail.com>
2022-10-27 13:38:36 +0530

    to do moderately finalized

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,35 +1,65 @@
-import React from 'react';
-import './ToDoList.css';
-import { useState } from 'react';
-import ListItem from './ListItem/ListItem';
+import React, { useState } from "react";
+import "./ToDoList.css";
+import MoreVertSharp from "@mui/icons-material/MoreVertSharp";
+import { Delete } from "@mui/icons-material";
 
 function ToDoList() {
+     const [handleForm, setHandleForm] = useState("");
 
-  const [currentItem, setCurrentItem] = useState(null);
-  const [itemList, updateItemList] = useState([{item: "Start UI", key: Math.random()*1000},{item: "Framework Setup", key: Math.random()*1000}, {item: "Firebase Setup", key: Math.random()*1000}]);
+     const [itemList, setItemList] = useState([
+          { name: "Start UI", key: Math.random() * 1000 },
+          { name: "Framework Setup", key: Math.random() * 1000 },
+          { name: "Firebase Setup", key: Math.random() * 1000 },
+     ]);
 
-  const onChangeHandler = e => {
-    setCurrentItem(e.target.value);
-  }
+     const addItemToList = () => {
+          if (handleForm !== "") {
+               setItemList([...itemList, { name: handleForm, key: Date.now() }]);
+          }
+          setHandleForm("");
+     };
 
-  const addItemToList = () => {
-    updateItemList([...itemList, { item: currentItem, key: Date.now() }]);
-    setCurrentItem("");
-  }
+     const deleteItem = (id) => {
+          setItemList(itemList.filter((item) => item.key !== id));
+     };
 
+     return (
+          <div className="todo-list-container">
+               <h3>Task to be completed</h3>
 
-  return (
-    <div className='to-do-list-container'>
-      <h3>Task to be completed</h3>
-      <div className='to-do-container'>
-        {/* <div className='input-container'>
-          <input value={currentItem} onChange={onChangeHandler} />
-        </div> */}
-        <ListItem itemList={itemList} updateItemList={updateItemList} />
-        <button id='addBtn' onClick={addItemToList}>Add Todo</button>
-      </div>
-    </div>
-  )
+               <div className="todo-main-section">
+                    <div className="list-container">
+                         {itemList.map((item) => {
+                              return (
+                                   <div className="item">
+                                        <div className="item-left">
+                                             <input type="radio" />
+                                             <p>{item.name}</p>
+                                        </div>
+                                        <button
+                                             className="delete-button"
+                                             onClick={() => deleteItem(item.key)}
+                                        >
+                                             <Delete />
+                                        </button>
+                                   </div>
+                              );
+                         })}
+                    </div>
+
+                    <input
+                         type="text"
+                         value={handleForm}
+                         onChange={(e) => {
+                              setHandleForm(e.target.value);
+                         }}
+                    />
+                    <button id="addBtn" onClick={addItemToList}>
+                         Add Todo
+                    </button>
+               </div>
+          </div>
+     );
 }
 
 export default ToDoList;
